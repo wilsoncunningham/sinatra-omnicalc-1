5abe05e3c9e01fa03fdf26faa2765e6a77882637
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-31 23:15:08 +0000

    done

--- app.rb
+++ app.rb
@@ -31,6 +31,22 @@ get("/payment/new") do
   erb(:payment)
 end
 
+get("/payment/results") do
+  @apr = params.fetch("user_apr").to_f / 100 / 12
+  @duration = params.fetch("user_duration").to_i
+  @principal =  params.fetch("user_principal").to_f
+
+  @apr_display = (@apr*100*12).to_fs(:percentage, {:precision => 4})
+  @principal_display = @principal.to_fs(:currency)
+
+  @periods = @duration * 12
+
+  @payment = (@apr * @principal) / (1 - (1 + @apr)**(-@periods))
+  @payment_display = @payment.to_fs(:currency)
+
+  erb(:payment_results)
+end
+
 get("/random/new") do
   erb(:random)
 end
